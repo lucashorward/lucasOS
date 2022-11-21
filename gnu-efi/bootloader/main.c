fb2d60aa5a889a8aa14965b98d970797c6b184ff
Lucas Horward <l.horward@essent.nl>
2022-11-21 21:18:26 +0100

    Completed ep 2

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -4,55 +4,66 @@
 
 typedef unsigned long long size_t;
 
-EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
-	EFI_FILE* LoadedFile;
+EFI_FILE *LoadFile(EFI_FILE *Directory, CHAR16 *Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+	EFI_FILE *LoadedFile;
 
-	EFI_LOADED_IMAGE_PROTOCOL* LoadedImage;
-	SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&LoadedImage);
+	EFI_LOADED_IMAGE_PROTOCOL *LoadedImage;
+	SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void **)&LoadedImage);
 
-	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* FileSystem;
-	SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)&FileSystem);
+	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem;
+	SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void **)&FileSystem);
 
-	if (Directory == NULL) {
+	if (Directory == NULL)
+	{
 		FileSystem->OpenVolume(FileSystem, &Directory);
 	}
 
 	EFI_STATUS s = Directory->Open(Directory, &LoadedFile, Path, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
 
-	if (s != EFI_SUCCESS) {
+	if (s != EFI_SUCCESS)
+	{
 		return NULL;
 	}
 	return LoadedFile;
 }
 
-int memcmp(const void* aptr, const void* bptr, size_t n) {
-	const unsigned char* a =aptr, *b = bptr;
-	for (size_t i = 0; i < n; i++) {
-		if (a[i] < b[i]) return -1;
-		else if (a[i] > b[i]) return 1;
+int memcmp(const void *aptr, const void *bptr, size_t n)
+{
+	const unsigned char *a = aptr, *b = bptr;
+	for (size_t i = 0; i < n; i++)
+	{
+		if (a[i] < b[i])
+			return -1;
+		else if (a[i] > b[i])
+			return 1;
 	}
 	return 0;
 }
 
-EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
 	InitializeLib(ImageHandle, SystemTable);
 	Print(L"Long string blah\n\r");
 
-	EFI_FILE* Kernel = LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable);
+	EFI_FILE *Kernel = LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable);
 
-	if (Kernel == NULL) {
+	if (Kernel == NULL)
+	{
 		Print(L"Could not load kernel\n\r");
-	} else {
+	}
+	else
+	{
 		Print(L"Kernel loaded succesfully\n\r");
 	}
 
-	Elf64_Ehdr header; 
+	Elf64_Ehdr header;
 	{
 		UINTN FileInfoSize;
-		EFI_FILE_INFO* FileInfo;
+		EFI_FILE_INFO *FileInfo;
 		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, NULL);
-		SystemTable->BootServices->AllocatePool(EfiLoaderData, FileInfoSize, (void**)&FileInfo);
-		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, (void**)&FileInfo);
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, FileInfoSize, (void **)&FileInfo);
+		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, (void **)&FileInfo);
 
 		UINTN size = sizeof(header);
 		Kernel->Read(Kernel, &size, &header);
@@ -64,12 +75,49 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 		header.e_ident[EI_DATA] != ELFDATA2LSB ||
 		header.e_type != ET_EXEC ||
 		header.e_machine != EM_X86_64 ||
-		header.e_version != EV_CURRENT
-	) {
+		header.e_version != EV_CURRENT)
+	{
 		Print(L"Kernel is borked\n\r");
-	} else {
+	}
+	else
+	{
 		Print(L"Kernel is pristine and verified\n\r");
 	}
+
+	Elf64_Phdr *phdrs;
+	{
+		Kernel->SetPosition(Kernel, header.e_phoff);
+		UINTN size = header.e_phnum * header.e_phentsize;
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, size, (void **)&phdrs);
+		Kernel->Read(Kernel, &size, phdrs);
+	}
+
+	for (
+		Elf64_Phdr *phdr = phdrs;
+		(char *)phdr < (char *)phdrs + header.e_phnum * header.e_phentsize;
+		phdr = (Elf64_Phdr *)((char *)phdr + header.e_phentsize))
+	{
+		switch (phdr->p_type)
+		{
+		case PT_LOAD:
+		{
+			int pages = (phdr->p_memsz + 0x1000 - 1) / 0x1000;
+			Elf64_Addr segment = phdr->p_paddr;
+			SystemTable->BootServices->AllocatePages(AllocateAddress, EfiLoaderData, pages, &segment);
+
+			Kernel->SetPosition(Kernel, phdr->p_offset);
+			UINTN size = phdr->p_filesz;
+			Kernel->Read(Kernel, &size, (void *)segment);
+			break;
+		}
+		}
+	}
+
+	Print(L"Kernel loading complete.\n\r");
+
+	int (*KernelStart)() = ((__attribute__((sysv_abi)) int (*)()) header.e_entry);
+
+	Print(L"%d\n\r", KernelStart());
 
 	return EFI_SUCCESS; // Exit the UEFI application
 }
