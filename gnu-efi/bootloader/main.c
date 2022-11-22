1c187f83dad877fc7429c98549ca3ad55506b7dc
Lucas Horward <l.horward@essent.nl>
2022-11-22 22:06:11 +0100

    Completed episode 3

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -4,6 +4,53 @@
 
 typedef unsigned long long size_t;
 
+typedef struct
+{
+	void *BaseAddress;
+	size_t BufferSize;
+	unsigned int Width;
+	unsigned int Height;
+	unsigned int PixelsPerScanline;
+} Framebuffer;
+
+#define PSF1_MAGIC0 0x36
+#define PSF1_MAGIC1 0x04
+
+typedef struct {
+	unsigned char magic[2];
+	unsigned char mode;
+	unsigned char charsize;
+} PSF1_HEADER;
+
+typedef struct {
+	PSF1_HEADER* psf1_Header;
+	void* glyphBuffer;
+} PSF1_FONT;
+
+Framebuffer framebuffer;
+
+Framebuffer *InitializeGOP()
+{
+	EFI_GUID gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+	EFI_STATUS status;
+
+	status = uefi_call_wrapper(BS->LocateProtocol, 3, &gopGuid, NULL, (void **)&gop);
+	if (EFI_ERROR(status))
+	{
+		Print(L"Unable to locate GOP\n\r");
+		return NULL;
+	}
+	Print(L"GOP located\n\r");
+	framebuffer.BaseAddress = (void *)gop->Mode->FrameBufferBase;
+	framebuffer.BufferSize = gop->Mode->FrameBufferSize;
+	framebuffer.Width = gop->Mode->Info->HorizontalResolution;
+	framebuffer.Height = gop->Mode->Info->VerticalResolution;
+	framebuffer.PixelsPerScanline = gop->Mode->Info->PixelsPerScanLine;
+
+	return &framebuffer;
+}
+
 EFI_FILE *LoadFile(EFI_FILE *Directory, CHAR16 *Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
 	EFI_FILE *LoadedFile;
@@ -26,6 +73,38 @@ EFI_FILE *LoadFile(EFI_FILE *Directory, CHAR16 *Path, EFI_HANDLE ImageHandle, EF
 		return NULL;
 	}
 	return LoadedFile;
+}
+
+PSF1_FONT* LoadPSF1Font(EFI_FILE *Directory, CHAR16 *Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+	EFI_FILE* font = LoadFile(Directory, Path, ImageHandle, SystemTable);
+	if (font == NULL) return NULL;
+
+	PSF1_HEADER* fontheader;
+	SystemTable->BootServices->AllocatePool(EfiLoaderData, sizeof(PSF1_HEADER), (void**)&fontheader);
+	UINTN size = sizeof(PSF1_HEADER);
+	font->Read(font, &size, fontheader);
+
+	if(fontheader->magic[0] != PSF1_MAGIC0 || fontheader->magic[1] != PSF1_MAGIC1) {
+		return NULL;
+	}
+
+	UINTN glyphBufferSize = fontheader->charsize * 256;
+	if (fontheader->mode == 1) {
+		glyphBufferSize = fontheader->charsize * 512;
+	}
+
+	void* glyphBuffer;
+	{
+		font->SetPosition(font, sizeof(PSF1_HEADER));
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, glyphBufferSize, (void**)&glyphBuffer);
+		font->Read(font, &glyphBufferSize, glyphBuffer);
+	}
+
+	PSF1_FONT* finishedFont;
+	SystemTable->BootServices->AllocatePool(EfiLoaderData, sizeof(PSF1_FONT), (void**)&finishedFont);
+	finishedFont->psf1_Header = fontheader;
+	finishedFont->glyphBuffer = glyphBuffer;
+	return finishedFont;
 }
 
 int memcmp(const void *aptr, const void *bptr, size_t n)
@@ -115,9 +194,20 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
 	Print(L"Kernel loading complete.\n\r");
 
-	int (*KernelStart)() = ((__attribute__((sysv_abi)) int (*)()) header.e_entry);
+	void (*KernelStart)(Framebuffer*, PSF1_FONT*) = ((__attribute__((sysv_abi)) void (*)(Framebuffer*, PSF1_FONT*))header.e_entry);
 
-	Print(L"%d\n\r", KernelStart());
+	PSF1_FONT* newFont = LoadPSF1Font(NULL, L"zap-light16.psf", ImageHandle, SystemTable);
+	if (newFont == NULL) {
+		Print(L"Font is not valid or not found\n\r");
+	} else {
+		Print(L"Font found. Char size is %d\n\r", newFont->psf1_Header->charsize);
+	}
+
+	Framebuffer *newBuffer = InitializeGOP();
+
+	Print(L"Base: 0x%x\n\rSize: 0x%x\n\rWidth: %d\n\rHeight: %d\n\rPixels per scan line: %d\n\r\n\r", newBuffer->BaseAddress, newBuffer->BufferSize, newBuffer->Width, newBuffer->Height, newBuffer->PixelsPerScanline);
+
+	KernelStart(newBuffer, newFont);
 
 	return EFI_SUCCESS; // Exit the UEFI application
 }
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,3 +1,69 @@
-int _start() {
-    return 123;
+typedef unsigned long long size_t;
+
+typedef struct
+{
+    void *BaseAddress;
+    size_t BufferSize;
+    unsigned int Width;
+    unsigned int Height;
+    unsigned int PixelsPerScanline;
+} Framebuffer;
+
+typedef struct
+{
+    unsigned char magic[2];
+    unsigned char mode;
+    unsigned char charsize;
+} PSF1_HEADER;
+
+typedef struct
+{
+    PSF1_HEADER *psf1_Header;
+    void *glyphBuffer;
+} PSF1_FONT;
+
+typedef struct {
+    unsigned int X;
+    unsigned int Y;
+} Point;
+
+void putChar(Framebuffer *frameBuffer, PSF1_FONT *psf1_font, unsigned int colour, char chr, unsigned int xOff, unsigned int yOff)
+{
+    unsigned int *pixPtr = (unsigned int *)frameBuffer->BaseAddress;
+    char *fontPtr = psf1_font->glyphBuffer + (chr * psf1_font->psf1_Header->charsize);
+    for (unsigned long y = yOff; y < yOff + 16; y++)
+    {
+        for (unsigned long x = xOff; x < xOff + 8; x++)
+        {
+            if ((*fontPtr & (0b10000000 >> (x - xOff))) > 0)
+            {
+                *(unsigned int *)(pixPtr + x + (y * frameBuffer->PixelsPerScanline)) = colour;
+            }
+        }
+        fontPtr++;
+    }
+}
+
+Point CursorPosition;
+void Print(Framebuffer *frameBuffer, PSF1_FONT *psf1_font, unsigned int colour, char* str) {
+    char* chr = str;
+    while (*chr != 0) {
+        putChar(frameBuffer, psf1_font, colour, *chr, CursorPosition.X, CursorPosition.Y);
+        CursorPosition.X += 8;
+        if (CursorPosition.X + 8 > frameBuffer->Width) {
+            CursorPosition.X = 0;
+            CursorPosition.Y += 16;
+        }
+        chr++;
+    }
+}
+
+void _start(Framebuffer *framebuffer, PSF1_FONT *psf1_font)
+{
+    CursorPosition.X = 50;
+    CursorPosition.Y = 120;
+    for (int i = 0; i < 50; i+=1) {
+        Print(framebuffer, psf1_font, 0xffffffff, "Hello kernel");
+    }
+    return;
 }
